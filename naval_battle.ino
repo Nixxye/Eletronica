08d8c1209a3c57018ae2de758427ff4eadc78dbb
Nixxye <128294977+Nixxye@users.noreply.github.com>
2023-06-15 14:50:45 -0300

    Add files via upload

--- naval_battle.ino
+++ naval_battle.ino
@@ -1,23 +1,29 @@
 #include <LedControl.h>
 
 //Constants
-#define MAX_SHIPS 3
+#define MAX_SHIPS 1
 
 //Keys
-#define LEFT 13
-#define RIGHT 7
-#define DOWN 8
+#define X 3
+#define Y 5
 #define SELECT 2
 //States -> todos os states do player1 terminam com 0 e do player 2, com 1
 #define MENU 19
 #define CREATING_PLAYER_1 20 
 #define CREATING_PLAYER_2 21
+#define ATTACKING 29
+#define ATTACKING_PLAYER_1 30
+#define ATTACKING_PLAYER_2 31
+#define WIN_PLAYER_1 40
+#define WIN_PLAYER_2 41
 
 LedControl lc = LedControl(12, 11, 10, 2);
 
 int x = 0, y = 0, selected = 0, state = MENU;
 
 int field[2][8][8];
+int attack_field[2][8][8];
+
 
 int ships_alive[2] = {0};
 
@@ -29,6 +35,8 @@ void setup()
     {
       field[0][i][j] = 0;
       field[1][i][j] = 0;
+      attack_field[0][i][j] = 0;
+      attack_field[1][i][j] = 0;
     }
   //Player 1:
   lc.shutdown(0,false);
@@ -39,10 +47,31 @@ void setup()
   lc.setIntensity(1,1);
   lc.clearDisplay(1);
   //Botões
-  pinMode(LEFT, INPUT);
-  pinMode(RIGHT, INPUT);
-  pinMode(DOWN, INPUT);
+  
   pinMode(SELECT, INPUT);
+
+  Serial.begin(9600);
+}
+void clearScreen(int player)
+{
+    if (player > 1 || player < 0)
+      return;
+    for (int i = 0; i < 8; i++)
+      for (int j = 0; j < 8; j++)
+      {
+        lc.setLed(player,i,j, false);  
+      }  
+}
+void printX(int player)
+{
+    if (player > 1 || player < 0)
+      return;
+    for (int i = 0; i < 8; i++)
+      for (int j = 0; j < 8; j++)
+      {
+        if (j = i || j == 7 - i)
+          lc.setLed(player,i,j, true);  
+      }
 }
 void transition()
 {
@@ -74,15 +103,21 @@ void getInput()
 {
   //Colocar para piscar na seleção:
   selected = 0;
-
-  if (digitalRead(RIGHT))
+  //Serial.print("eitatsts");
+  /*
+  Serial.print("x:");
+  Serial.println(analogRead(X));
+  Serial.print("y:");
+  Serial.println(analogRead(Y));
+  */
+  if (analogRead(X) > 900)
   {
     printSelection(state % 10, 0);
     x = (x + 1) % 8;
     printSelection(state % 10, 1);
     delay(500);
   }
-  else if (digitalRead(LEFT))
+  else if (analogRead(X) < 100)
   {
     printSelection(state % 10, 0);
     x = x - 1;
@@ -91,10 +126,19 @@ void getInput()
     printSelection(state % 10, 1);
     delay(500);
   }
-  else if (digitalRead(DOWN))
+  else if (analogRead(Y) > 900)
   {
     printSelection(state % 10, 0);
     y = (y + 1) % 8;
+    printSelection(state % 10, 1);
+    delay(500);
+  }
+  else if (analogRead(Y) < 100)
+  {
+    printSelection(state % 10, 0);
+    y = y - 1;
+    if(y < 0)
+      y = 7;
     printSelection(state % 10, 1);
     delay(500);
   }
@@ -104,56 +148,45 @@ void getInput()
     delay(500);
   }
 }
-void print()
+void printField(int player)
 {
-  switch (state)
-  {
-    /*
-    case CREATING_PLAYER_1:
-      for (int i = 0; i < 8; i++)
-        for (int j = 0; j < 8; j++)
-        {
-          if (field[0][i][j])
-            lc.setLed(0,i,j, true);
-          else 
-            lc.setLed(0,i,j, false);
-        }
-      break;
-    case CREATING_PLAYER_2:
-      for (int i = 0; i < 8; i++)
-        for (int j = 0; j < 8; j++)
-        {
-          if (field[1][i][j])
-            lc.setLed(1,i,j, true);
-          else 
-            lc.setLed(1,i,j, false);
-        }
-      break;
-      */
-    default:
-        for (int i = 0; i < 8; i++)
-          for (int j = 0; j < 8; j++)
-          {
-            if (field[0][i][j])
-              lc.setLed(0,i,j, true);
-            else 
-              lc.setLed(0,i,j, false);
-            if (field[1][i][j])
-              lc.setLed(1,i,j, true);
-            else 
-              lc.setLed(1,i,j, false);
-        }
-      break;
-  } 
-  return;
+   if (player > 1 || player < 0)
+    return; 
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
+    {
+      if (field[player][i][j])
+        lc.setLed(player,i,j, true);
+      else 
+        lc.setLed(player,i,j, false);
+    }        
 } 
-
+void printAttack(int player)
+{
+   if (player > 1 || player < 0)
+    return; 
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
+    {
+      if (i == 0 || i == 3)
+        lc.setLed(player,i,j, true);
+      else if (j == 0 || j == 7)
+        lc.setLed(player,i,j, true);
+    }
+  delay(500);
+  clearScreen(player);
+}
 void createField(int player)
 {
   if (player > 1 || player < 0)
     return;
   while (true)
   {
+    if (ships_alive[player] >= MAX_SHIPS)
+    {
+      transition();
+      return;
+    }  
     getInput();
     if (selected)
     {
@@ -162,14 +195,124 @@ void createField(int player)
         field[player][y][x] = 1;
         ships_alive[player]++;
       }
-      if (ships_alive[player] >= MAX_SHIPS)
+    }
+    printField(player);
+    delay(10);
+  }
+}
+void printAttackField(int player)
+{
+  if (player > 1 || player < 0)
+    return; 
+  for (int i = 0; i < 8; i++)
+    for (int j = 0; j < 8; j++)
+    {
+      if (attack_field[player][i][j])
+        lc.setLed(player,i,j, true);
+      else 
+        lc.setLed(player,i,j, false);
+    }  
+}
+void attack()
+{
+  while (ships_alive[0] > 0 && ships_alive[1] > 0)
+  {
+      printAttack(0);
+      printField(1);
+      state = ATTACKING_PLAYER_1;
+      while(true)
       {
-        transition();
+        getInput();
+        if (selected)
+        {
+          if (attack_field[0][y][x] == 0)
+          {
+            attack_field[0][y][x] = 1;
+            if (field[1][y][x] == 1)
+            {
+              ships_alive[1]--;
+            }
+            field[1][y][x] = 0;
+            break;
+          }
+        }
+        printAttackField(0);
+      }
+      if (ships_alive[1] < 0)
+      {
+        state = WIN_PLAYER_1;
         return;
       }
+      state = ATTACKING_PLAYER_2;
+      printAttack(1);
+      printField(0);
+      while (true)
+      {
+        getInput();
+        if (selected)
+        {
+          if (attack_field[1][y][x] == 0)
+          {
+            attack_field[1][y][x] = 1;
+            if (field[0][y][x] == 1)
+            {
+              ships_alive[0]--;
+            }
+            field[0][y][x] = 0;
+            break;
+          }
+        }
+        printAttackField(1);
+      }
+      if (ships_alive[0] <= 0)
+      {
+        state = WIN_PLAYER_2;
+        return;
+      }
+  } 
+  
+  //Provavelmente irá sair:
+  if (ships_alive[0] <= 0)
+  {
+    state = WIN_PLAYER_2;
+    return;
+  }
+  else if (ships_alive[1] < 0)
+  {
+    state = WIN_PLAYER_1;
+    return;
+  }
+}
+void print_win(int player)
+{
+  for (int t = 0; t < 4; t++)
+  {
+    for (int i = t; i < 8 - t; i++)
+    {
+      lc.setLed(player,i,t, true);
+      lc.setLed(player,i,7-t, true);
+      delay(10);
     }
-    print();
-    delay(10);
+    for (int j = t; j < 8 - t; j++)
+    {
+      lc.setLed(player,t,j, true);
+      lc.setLed(player,7-t,j, true);
+      delay(10);
+    }   
+    delay(100);
+  }
+  delay(100);
+  clearScreen(player);
+}
+void win(int player)
+{
+  if (player > 1 || player < 0)
+    return; 
+  selected = 0;
+  while(!selected)
+  {
+    getInput();
+    print_win(player);
   }
 }
 void stateManager()
@@ -186,8 +329,16 @@ void stateManager()
       break;
     case CREATING_PLAYER_2:
       createField(1);
-      //Apenas para testes:
-      state = MENU;
+      state = ATTACKING;
+      break;
+    case ATTACKING:
+      attack();
+      break;
+    case WIN_PLAYER_1:
+      win(0);
+      break;
+    case WIN_PLAYER_2:
+      win(1);
       break;
     default:
       break;
